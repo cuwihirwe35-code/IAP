4bbdd1dd2f2daa9f5bbb77545fb41cb9ce11e704
collegio <collegio@gmail.com>
2026-03-23 17:26:45 +0200

    üpdates

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+require('dotenv').config();
+const express= require('express');
+const PORT = process.env.PORT;
+const app=express();
+app.use(express.json());
+const db=require('./config/db')
+app.listen(PORT, () => {
+  console.log(` server is running on port ${PORT}`);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+});
+module.exports = app;
